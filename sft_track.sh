2bf601aa1c4416d59ce282f4284b09a4f88e8df5
Tony Garcia <tonysk8@gmx.net>
2024-06-02 16:42:22 -0500

    Refactor to use new repo

--- sft_track.sh
+++ sft_track.sh
@@ -12,80 +12,90 @@
 #~ NOTE: AUR packages make use of RPMs.
 
 _VERSION=0.0.1
-FULL_NAME=$(basename $0)
-NAME=$(basename $0  | cut -d. -f1)
+FULL_NAME=$(basename "$0")
+NAME=$(basename "$0"  | cut -d. -f1)
+
 # GIT_AUR_PATH is used to use it as the location where the repos are located
 
 aur_url="https://aur.archlinux.org/packages"
 
 # Required scripts
-UPDPKGSUMS=/bin/updpkgsums
 MAKEPKG=/bin/makepkg
 GIT=/bin/git
 if [[ ! -x "${MAKEPKG}" ||
       ! -x "${GIT}" ]]; then
-  echo "One or more tools required by this script are mising:" >&2
-  echo "${MAKEPKG}, ${GIT}" >&2
-  #exit 1
+    echo "One or more tools required by this script are mising:" >&2
+    echo "${MAKEPKG}, ${GIT}" >&2
+    #exit 1
 fi
 
 # Tracking a package
 track_pkg() {
-  local pkg=$1
-  local flags=$2
-  local latest_rpm_pkg=$( ./sft_ver.sh ${flags} )
-  local aur_pkg=$( curl -sL ${aur_url}/${pkg}/ | grep -Po "${pkg} [\d+.-]+" )
-  # Getting versions(as one single number)
-  local aur_num_ver=$( echo ${aur_pkg%-*} | grep -oP "\d" | paste -sd '' )
-  local latest_rpm_num_ver=$( echo ${latest_rpm_pkg%-*} |
-                                grep -oP "\d" |
-                                paste -sd '' )
-  if [[ ${latest_rpm_num_ver} -gt ${aur_num_ver} ]]; then
-    echo "${latest_rpm_pkg%-*}" | sed -e 's/^.*-//'
-    return 1
-  else
-    return 0
-  fi
+    local pkg=$1
+    local flags=$2
+    local latest_rpm_ver
+    local aur_pkg
+    local aur_num_ver
+    local latest_rpm_num_ver
+
+    latest_rpm_ver=$( ./sft_ver.sh -v )
+    aur_pkg=$( curl -sL "${aur_url}/${pkg}/" | grep -Po "${pkg} [\d+.-]+" )
+
+    # Getting versions(as one single number)
+    aur_num_ver=$( echo "${aur_pkg%-*}" | grep -oP "\d" | paste -sd '' )
+    latest_rpm_num_ver=$( echo "${latest_rpm_ver}" |
+                              grep -oP "\d" |
+                              paste -sd '' )
+
+    # Validate the rpm  is available
+    #echo "${sft_repo}/rpm/stable/${BASE_OS}/${BASE_OS_VER}/x86_64/${sft_pkg}-${latest_ver}.x86_86.rpm"
+    if [[ ${latest_rpm_num_ver} -gt ${aur_num_ver} ]]; then
+        echo "${latest_rpm_ver}"
+        return 1
+    else
+        return 0
+    fi
 }
 
 # Help
 print_help(){
-  echo "Usage: ${FULL_NAME} [-h|-v]|[[-d][-r]] [[-c][-u][-s]]"
-  grep -E '^#~' $0|sed -e 's/^#~//'
+    echo "Usage: ${FULL_NAME} [-h|-v]|[[-d][-r]] [[-c][-u][-s]]"
+    grep -E "^#~" "$0" |
+        sed -e "s/^#~//"
 }
 
 
 # AUR packages use RPMs, track RPM versions:
-flags="-r"
+flags="-"
 pkg=""
 while getopts chsuv arg; do
-  case ${arg} in
-    h)
-      print_help
-      exit 0
-      ;;
-    c)
-      pkg="scaleft-client-tools-bin"
-      flags+="c"
-      ;;
-    u)
-      pkg="scaleft-url-handler"
-      flags+="u"
-      ;;
-    s)
-      pkg="scaleft-server-tools-bin"
-      flags+="s"
-      ;;
-    v)
-      echo "${NAME} ${_VERSION}"
-      exit 0
-      ;;
-   esac
+    case ${arg} in
+        c)
+            pkg="scaleft-client-tools-bin"
+            flags+="v"
+            ;;
+        u)
+            pkg="scaleft-url-handler"
+            flags+="v"
+            ;;
+        s)
+            pkg="scaleft-server-tools-bin"
+            flags+="v"
+            ;;
+        v)
+            echo "${NAME} ${_VERSION}"
+            exit 0
+            ;;
+        h|*)
+            print_help
+            exit 0
+            ;;
+    esac
 done
 
 get_sha() {
   source=${1}
-  curl -s ${source} |
+  curl -s "${source}" |
     sha256sum |
     awk '{print $1}'
 }
@@ -93,7 +103,7 @@ get_sha() {
 # AUR repo + pkg name
 REPO_DIR="${GIT_AUR_PATH}/${pkg}"
 
-if [[ ! -z "${pkg}" ]]; then
+if [[ -n "${pkg}" ]]; then
   newest_ver=$( track_pkg "${pkg}" "${flags}" )
   if [[ -n ${newest_ver} ]]; then
     ls -ld "${REPO_DIR}"
@@ -101,21 +111,26 @@ if [[ ! -z "${pkg}" ]]; then
      echo "Error, git directory not found: ${REPO_DIR}" >&2
      exit 1
     fi
-    pushd ${REPO_DIR}
+    pushd "${REPO_DIR}" || exit 1
 
     # Replace version
     sed -i "s/^\(pkgver=\).*/\1${newest_ver}/" PKGBUILD
 
     # Replace sha256sum
-    source PKGBUILD
-    new_sha=$(get_sha ${source})
+    if [[ -r PKGBUILD ]]; then
+        # shellcheck source=/dev/null
+        source PKGBUILD
+    else
+        echo "Error: No PKGBUILD found" >&2
+        exit
+    fi
+    new_sha=$(get_sha "${source}")
     sed -i "s/^\(sha256sums=\).*/\1('${new_sha}')/" PKGBUILD
 
     makepkg --printsrcinfo | tee .SRCINFO
     makepkg --clean --force
-    echo git commit -m\"Updating package to v. ${newest_ver}\" .
+    echo "git commit -m\"Updating package to v. ${newest_ver}\" ."
     echo git push origin
-    echo rm -Rf ${tmp}
   fi
 fi
 
--- sft_ver.sh
+++ sft_ver.sh
@@ -1,189 +1,92 @@
 #!/bin/bash
-# Getting the latest version for sft packages.
+# Get and validate the sft latest packages
 # Tony G. <aur at tonyskapunk dot net>
 #
 #~ Options:
 #~   -a    Query all.
 #~   -c    Query client packages.
-#~   -d    Query "deb" packages.
-#~   -D    Query all "deb" packages.
 #~   -h    Print this help.
-#~   -r    Query "rpm" packages.
-#~   -r    Query all "rpm" packages.
 #~   -s    Query server packages.
 #~   -u    Query url-handler packages.
-#~   -v    Print the Version.
+#~   -v    Print the latest version of the packages.
+#~   -V    Print the Version.
 
-_VERSION=0.0.1
-FULL_NAME=$(basename $0)
-NAME=$(basename $0  | cut -d. -f1)
+_VERSION=1.0.0
+FULL_NAME=$(basename "$0")
+NAME=$(basename "$0"  | cut -d. -f1)
+BASE_OS=centos
+BASE_OS_VER=9
+ARCH=x86_64
+SFT_REPO=https://dist.scaleft.com/repos
+SFT_URL=${SFT_REPO}/rpm/stable/${BASE_OS}/${BASE_OS_VER}/${ARCH}/
 
-sft_repo=https://pkg.scaleft.com
 sft_client=scaleft-client-tools
 sft_handler=scaleft-url-handler
 sft_server=scaleft-server-tools
 
-# Latest clients
-get_latest_rpm_client() {
-  local latest_pkg=$( curl -s ${sft_repo}/rpm/ 2>/dev/null |
-                        grep -Po "${sft_client}[\w\d.-]*" |
-                        sort -V |
-                        tail -1
-                    )
-  echo ${latest_pkg}
+# Latest version
+get_latest_version() {
+    local latest_ver
+    latest_ver=$( curl -s ${SFT_URL} 2>/dev/null |
+                    grep -Po '>(\d+\.?){3}<' |
+                    grep -Po "(\d+\.?){3}" |
+                    sort -V |
+                    tail -1
+                )
+    echo "${latest_ver}"
 }
 
-get_latest_deb_client() {
-  local deb_pkg=pool/linux/main/s/${sft_client}
-  local latest_pkg=$( curl -s ${sft_repo}/deb/${deb_pkg}/ 2>/dev/null |
-                        grep -Po 'scale[\w\d.-]*' |
-                        sort -V |
-                        tail -1
-                    )
-  echo ${latest_pkg}
+# Latest packages
+get_latest_pkg() {
+    local sft_pkg="$1"
+    local latest_ver
+    latest_ver=$(get_latest_version)
+    if curl -s -o /dev/null -w "%{response_code}" "${SFT_URL}" | grep -q "200"; then
+        echo "${sft_pkg}-${latest_ver}.${ARCH}.rpm"
+    else
+        echo "Error: ${sft_pkg} not found in version ${latest_ver}" >&2
+        exit 1
+    fi
 }
-
-
-# Latest handlers
-get_latest_rpm_handler() {
-  local latest_pkg=$( curl -s ${sft_repo}/rpm/ 2>/dev/null |
-                        grep -Po "${sft_handler}[\w\d.-]*" |
-                        sort -V |
-                        tail -1
-              )
-  echo ${latest_pkg}
-}
-
-get_latest_deb_handler() {
-  local deb_pkg=pool/linux/main/s/${sft_handler}
-  local latest_pkg=$( curl -s ${sft_repo}/deb/${deb_pkg}/ 2>/dev/null |
-                        grep -Po 'scale[\w\d.-]*' |
-                        sort -V |
-                        tail -1
-                    )
-  echo ${latest_pkg}
-}
-
-
-# Latest server
-get_latest_rpm_server() {
-  local latest_pkg=$( curl -s ${sft_repo}/rpm/ |
-                        grep -Po "${sft_server}[\w\d.-]*" 2>/dev/null |
-                        sort -V |
-                        tail -1
-              )
-  echo ${latest_pkg}
-}
-
-get_latest_deb_server() {
-  local deb_pkg=pool/linux/main/s/${sft_server}
-  local latest_pkg=$( curl -s ${sft_repo}/deb/${deb_pkg}/ 2>/dev/null |
-                        grep -Po 'scale[\w\d.-]*' |
-                        sort -V |
-                        tail -1
-              )
-  echo ${latest_pkg}
-}
-
 
 # Help
 print_help(){
-  echo "Usage: ${FULL_NAME} [-h|-v]|[[-d][-r]] [[-c][-u][-s]]"
-  grep -E '^#~' $0|sed -e 's/^#~//'
+    echo "Usage: ${FULL_NAME} [-h|-V]|[[-a][-c][-u][-s][-v]]"
+    grep -E "^#~" "$0" |
+      sed -e "s/^#~//"
 }
 
 
 # Main
-get_deb=0
-get_rpm=0
-get_client=0
-get_urlhandler=0
-get_server=0
-while getopts acDdhsRruv arg; do
-  case ${arg} in
-    a)
-      get_deb=1
-      get_rpm=1
-      get_client=1
-      get_urlhandler=1
-      get_server=1
-      ;;
-    h)
-      print_help
-      exit 0
-      ;;
-    d)
-      get_deb=1
-      ;;
-    r)
-      get_rpm=1
-      ;;
-    D)
-      get_deb=1
-      get_client=1
-      get_urlhandler=1
-      get_server=1
-      ;;
-    R)
-      get_rpm=1
-      get_client=1
-      get_urlhandler=1
-      get_server=1
-      ;;
- 
-    c)
-      get_client=1
-      ;;
-    u)
-      get_urlhandler=1
-      ;;
-    s)
-      get_server=1
-      ;;
-    v)
-      echo "${NAME} ${_VERSION}"
-      exit 0
-      ;;
-   esac
+while getopts achsuvV arg; do
+    case ${arg} in
+        a)
+            get_latest_pkg ${sft_client}
+            get_latest_pkg ${sft_handler}
+            get_latest_pkg ${sft_server}
+            
+            ;;
+        c)
+            get_latest_pkg ${sft_client}
+            ;;
+        u)
+            get_latest_pkg ${sft_handler}
+            ;;
+        s)
+            get_latest_pkg ${sft_server}
+            ;;
+        V)
+            echo "${NAME} ${_VERSION}"
+            exit 0
+            ;;
+        v)
+            get_latest_version
+            ;;
+        h|*)
+            print_help
+            exit 0
+            ;;
+    esac
 done
-
-if [[ ${get_deb} == 0 &&
-      ${get_rpm} == 0 ]]; then
-   echo "Need at least one kind of package -r(rpm) or -d(deb)."
-   exit 1
-fi
-if [[ ${get_client} == 0 &&
-      ${get_urlhandler} == 0 &&
-      ${get_server} == 0 ]]; then
-   echo "Need at least one type of package -c(client) or -u(handler) or"\
-        "-s(server)."
-   exit 1
-fi
-
-
-
-if [[ ${get_deb} == 1 ]]; then
-  if [[ ${get_client} == 1 ]]; then
-    get_latest_deb_client
-  fi
-  if [[ ${get_urlhandler} == 1 ]]; then
-    get_latest_deb_handler
-  fi
-  if [[ ${get_server} == 1 ]]; then
-    get_latest_deb_server
-  fi
-fi
-if [[ ${get_rpm} == 1 ]]; then
-  if [[ ${get_client} == 1 ]]; then
-    get_latest_rpm_client
-  fi
-  if [[ ${get_urlhandler} == 1 ]]; then
-    get_latest_rpm_handler
-  fi
-  if [[ ${get_server} == 1 ]]; then
-    get_latest_rpm_server
-  fi
-
-fi
 
 exit 0
